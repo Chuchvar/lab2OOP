e10a8801d1ea00697cda793e0479024d182769ff
Chuchvar <bogdan5.wap@gmail.com>
2023-10-12 12:04:33 +0300

    1

---
+++ src/PloKva.java
@@ -0,0 +1,11 @@
+public class PloKva {
+    int storn1;
+
+    public PloKva(int storn1) {
+        this.storn1 = storn1;
+    }
+     double plo(){
+        double plopt=Math.pow(storn1,2);
+        return plopt;
+    }
+}
